87dc09809f9976dda4f8f094ed0de191e70c5db1
Marco-Aguiar <marcoaguiar14@gmail.com>
2024-05-09 15:59:26 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AccountStatusModule } from './modules/AccountStatus.module';
 import { UserTypeModule } from './modules/UserType.module';
 import { ProductModule } from './modules/Product.module';
 import { LockStatusModule } from './modules/LockStatus.module';
+import { ExpectedRouteModule } from './modules/ExpectedRoute.module';
+import { TracedRouteModule } from './modules/TracedRoute.module';
 
 @Module({
-  imports: [DatabaseModule, AccountStatusModule, UserTypeModule, ProductModule, LockStatusModule],
+  imports: [DatabaseModule, AccountStatusModule, UserTypeModule, ProductModule, LockStatusModule, ExpectedRouteModule, TracedRouteModule],
   controllers: [],
   providers: [],
 })
