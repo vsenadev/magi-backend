3049673c01166a4623fbff3a43646477ee77bb78
vsenadev <senavinicius01@gmail.com>
2024-05-28 18:38:32 -0300

    FIX - Tracked and expect route correction

--- src/repository/Delivery.repository.ts
+++ src/repository/Delivery.repository.ts
@@ -6,7 +6,7 @@ import { DeliveryModelName } from '../schema/Delivery.schema';
 import { Delivery } from '../model/Delivery.model';
 import {
   IDelivery,
-  IDeliveryWithStatusCode,
+  IDeliveryWithStatusCode, IExpectedRoute, ITracedRoute,
 } from '../interface/Delivery.interface';
 
 @Injectable()
@@ -19,7 +19,6 @@ export class DeliveryRepository {
   createDelivery(body: IDelivery): Promise<IMessage> {
     return this.deliveryModel
       .findOne({ _id: body._id })
-    return this.DeliveryModel.findOne({ _id: body._id })
       .then((existingDelivery) => {
         if (existingDelivery) {
           return {
@@ -47,24 +46,6 @@ export class DeliveryRepository {
                 message: 'Entrega criada com sucesso!',
               };
             });
-          return this.DeliveryModel.create({
-            name: body.name.toUpperCase(),
-            sender: body.sender,
-            sendDate: body.sendDate,
-            expectedDate: body.expectedDate,
-            status: body.status,
-            products: body.products,
-            lockStatus: body.lockStatus,
-            expectedRoute: body.expectedRoute,
-            tracedRoute: body.tracedRoute,
-            startingAddress: body.startingAddress,
-            destination: body.destination,
-          }).then((): IMessage => {
-            return {
-              status: 201,
-              message: 'Entrega criada com sucesso!',
-            };
-          });
         }
       })
       .catch((error) => {
@@ -76,20 +57,16 @@ export class DeliveryRepository {
     return this.deliveryModel
       .find({}, { _id: 0, __v: 0 })
       .then((deliveries: IDelivery[]) => {
-    return this.DeliveryModel.find({}, { _id: 0, __v: 0 }).then(
-      (deliveries: IDelivery[]) => {
         return {
           status: 200,
           deliveries: deliveries,
         };
-      },
-    );
+      });
   }
 
   alterDelivery(_id: string, body: IDelivery): Promise<IMessage> {
     return this.deliveryModel
       .findOne({ _id: _id })
-    return this.DeliveryModel.findOne({ _id: _id })
       .then((existingDelivery) => {
         if (!existingDelivery) {
           return {
@@ -122,29 +99,6 @@ export class DeliveryRepository {
                 message: 'Entrega atualizada com sucesso!',
               };
             });
-          return this.DeliveryModel.updateOne(
-            {
-              _id: _id,
-            },
-            {
-              name: body.name.toUpperCase(),
-              sender: body.sender,
-              sendDate: body.sendDate,
-              expectedDate: body.expectedDate,
-              status: body.status,
-              products: body.products,
-              lockStatus: body.lockStatus,
-              expectedRoute: body.expectedRoute,
-              tracedRoute: body.tracedRoute,
-              startingAddress: body.startingAddress,
-              destination: body.destination,
-            },
-          ).then((): IMessage => {
-            return {
-              status: 201,
-              message: 'Entrega atualizada com sucesso!',
-            };
-          });
         }
       })
       .catch((error) => {
@@ -155,7 +109,6 @@ export class DeliveryRepository {
   deleteDelivery(_id: string): Promise<IMessage> {
     return this.deliveryModel
       .findOne({ _id: _id })
-    return this.DeliveryModel.findOne({ _id: _id })
       .then((existingDelivery) => {
         if (!existingDelivery) {
           return {
@@ -166,53 +119,55 @@ export class DeliveryRepository {
           return this.deliveryModel
             .deleteOne({ _id: _id })
             .then((): IMessage => {
-          return this.DeliveryModel.deleteOne({ _id: _id }).then(
-            (): IMessage => {
               return {
                 status: 201,
                 message: 'Entrega excluída com sucesso!',
               };
-            },
-          );
+            });
         }
       })
       .catch((error) => {
         throw new Error(error);
       });
   }
-      
+
   getExpectedRouteById(
     id: string,
-  ): Promise<{ status: number; expectedRoute: string }> {
+  ): Promise<{ status: number; expectedRoute: IExpectedRoute[] | unknown }> {
     return this.deliveryModel
-      .findOne({ _id: id }, { _id: 0, expectedRoute: 1 })
+      .findOne(
+        {
+          _id: id,
+        },
+        {
+          _id: 0,
+          name: 0,
+          sender: 0,
+          sendDate: 0,
+          expectedDate: 0,
+          status: 0,
+          products: 0,
+          lockStatus: 0,
+          tracedRoute: 0,
+          startingAddress: 0,
+          destination: 0,
+        },
+      )
       .then((delivery) => {
-        if (!delivery) {
-          return {
-            status: 404,
-            expectedRoute: null,
-          };
-        }
-        if (!delivery.expectedRoute || !Array.isArray(delivery.expectedRoute)) {
-          return {
-            status: 200,
-            expectedRoute: '', 
-          };
-        }
-        const expectedRouteString = delivery.expectedRoute.map(coord => `${coord.latitude},${coord.longitude}`).join(';');
         return {
           status: 200,
-          expectedRoute: expectedRouteString,
+          expectedRoute: delivery,
         };
       })
       .catch((error) => {
         throw new Error(error);
       });
   }
-  
-  
 
-  saveTrackedRouteById(id: string, trackedRoute: string): Promise<IMessage> {
+  saveTrackedRouteById(
+    id: string,
+    trackedRoute: ITracedRoute,
+  ): Promise<IMessage> {
     return this.deliveryModel
       .findOne({ _id: id })
       .then((delivery) => {
@@ -223,7 +178,7 @@ export class DeliveryRepository {
           };
         } else {
           return this.deliveryModel
-            .updateOne({ _id: id }, { tracedRoute: trackedRoute })
+            .updateOne({ _id: id }, { $push: { tracedRoute: trackedRoute } })
             .then((): IMessage => {
               return {
                 status: 201,
